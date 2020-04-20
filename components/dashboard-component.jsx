5ff4c448294bb6a50030cef88051b11fe709e337
Ricardo Vela <aricav96@gmail.com>
2020-04-20 10:17:58 -0500

    Separar encabezado de dashboard

--- components/dashboard-component.jsx
+++ components/dashboard-component.jsx
@@ -1,21 +1,14 @@
 import Box from '@material-ui/core/Box'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
+import Header from './header'
 
-const DashboardComponent = () => (
+const DashboardComponent = () => {
+  return (
     <Box>
-      <AppBar position="static">
-        <Toolbar>
-          <Button color="inherit">Borrar Cuenta</Button>
-          <Typography variant="h6">
-            Nombre de Equipo
-          </Typography>
-          <Button color="inherit">Logout</Button>
-        </Toolbar>
-      </AppBar>
+      <Header/>
+      <main>
+      </main>
     </Box>
-);
+  );
+}
 
 export default DashboardComponent;
