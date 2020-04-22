8b5da41ca5e205f5b915a4d3555c99c5fa86fc15
napoleon lazo <napoleon.lazo@hotmail.com>
2020-04-21 21:13:43 -0500

    Added Horario Component and buttons to config page

---
+++ pages/config.jsx
@@ -0,0 +1,10 @@
+import ConfigComponent from '../components/config-component'
+import { Grid } from '@material-ui/core';
+
+const Configuration = () => {
+  return (
+    <ConfigComponent/>
+  )
+}
+
+export default Configuration;
