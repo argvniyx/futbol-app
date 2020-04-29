8e40efcd453c2ba51b32690544ee26f956e9437a
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2020-04-28 20:23:36 -0500

    Modified middleware for parents
    Creates a user of type parent if the incoming request couldnt find user

--- functions/Middlewares/isParent.js
+++ functions/Middlewares/isParent.js
@@ -8,8 +8,27 @@ module.exports = (req, res, next) => {
         'users'
     ).doc(uid).get().then((snapshot) => {
         let user = snapshot.data();
-        if (user.TypeUser === 3) {
+        const TypeUser = 3;
+        if (snapshot.exists && user.TypeUser === 3) {
             next();
+        } else if(
+            // this is for the parent doesnt exist on our user
+            // table
+            !snapshot.exists
+        ) {
+            // add to our user table
+            admin.firestore().collection(
+                'users'
+            ).doc(uid).set({
+                uid: uid,
+                TypeUser: TypeUser,
+                children: []
+            }).then(() => {
+                next();
+            }).catch((error) => {
+                return res.status(500).json(error.message);
+            });
+
         } else {
             return res.status(401).send('You are not authorized to do that');
         }
