7d61a51c18d4ec46bb82526340991dd778f353d0
Ricardo Vela <aricav96@gmail.com>
2020-04-29 16:15:29 -0500

    Rudimentary progress on dashboard layout
    TODO:
    - Fix size disparity between Papers holding components
    - Fix Grid sizes so as to have a 4x4 style layout

--- components/dashboard-component.jsx
+++ components/dashboard-component.jsx
@@ -1,15 +1,21 @@
 import Box from '@material-ui/core/Box'
-import Header from './header'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core';
+import Header from './header'
+import UserCard from '../components/user-card'
+import EventDetails from '../components/event-details'
+import Directory from '../components/directory'
+import Timeline from '../components/timeline'
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column'
-  },
   content: {
-    display: 'flex',
-    flexGrow: 1
+    height: '100vh',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
   }
 }));
 
@@ -20,7 +26,30 @@ const DashboardComponent = props => {
     <Box className={classes.root}>
       <Header/>
       <main className={classes.content}>
-        {props.children}
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={8} lg={9}>
+              <Paper>
+                <Timeline />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={4} lg={3}>
+              <Paper>
+                <EventDetails />
+              </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                <Directory />
+              </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                <UserCard />
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
       </main>
     </Box>
   );
--- components/header.jsx
+++ components/header.jsx
@@ -10,9 +10,9 @@ import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
-  // root: {
-  //   flexGrow: 1,
-  // },
+  root: {
+    flexGrow: 0,
+  },
   formControl: {
     margin: theme.spacing(1),
     minWidth: 120,
