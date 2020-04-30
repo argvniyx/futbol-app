d56e90343be84f23b64ce4a51fc04da51e92305f
Ricardo Vela <aricav96@gmail.com>
2020-04-29 21:42:08 -0500

    Change theme colors

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ import { red } from '@material-ui/core/colors';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#222',
     },
     secondary: {
       main: '#19857b',
@@ -14,7 +14,7 @@ const theme = createMuiTheme({
       main: red.A400,
     },
     background: {
-      default: '#fff',
+      default: '#eee',
     },
   },
 });
