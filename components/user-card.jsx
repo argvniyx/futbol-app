e0d811be45e0af1e44b0bd09b2e68d9b55e7d05a
Ricardo Vela <aricav96@gmail.com>
2020-05-02 20:40:33 -0500

    Change UserCard to use a person props

--- components/user-card.jsx
+++ components/user-card.jsx
@@ -30,34 +30,38 @@ const useStyles = makeStyles((theme) => ({
   avatar: {
     width: theme.spacing(12),
     height: theme.spacing(12),
-    margin: theme.spacing(1)
+    margin: theme.spacing(1),
+    fontSize: theme.spacing(5)
   },
 }));
 
 export default function UserCard(props) {
   const classes = useStyles();
+  const {firstName, lastName, email, phone, children} = props.person;
+  const fullName = firstName + " " + lastName;
+  const childFullName = children[0].firstName + " " + children[0].lastName
 
   return (
     <Card className={props.className}>
       <CardContent className={classes.content}>
         <div className={classes.info}>
           <Typography component="h5" variant="h5">
-            Andrés Ricardo Garza Vela
+            {fullName}
           </Typography>
           <Typography variant="subtitle1" color="textSecondary">
-            aricav96@gmail.com
+            {email}
           </Typography>
           <Typography variant="subtitle2" color="textSecondary">
-            8166699777
+            {phone}
           </Typography>
           <Typography variant="body2">
-            Hijo: Fulano
+            Hijo: {childFullName}
           </Typography>
           <Typography variant="subtitle2">
-            Faltas: 2
+            Faltas: {children[0].absenceCount}
           </Typography>
         </div>
-        <Avatar className={classes.avatar} alt="Ricardo Vela"/>
+        <Avatar className={classes.avatar} alt={fullName} src="/broken.jpg"/>
       </CardContent>
       <CardActions>
         <IconButton>
