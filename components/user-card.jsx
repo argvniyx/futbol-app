01eb2c13c2e411642450fbe1a443047d3ffb2219
Ricardo Vela <aricav96@gmail.com>
2020-05-02 19:54:03 -0500

    Change UserCard layout. Add edit button
    The Edit button will probably either load a modal with fields to change
    the information or trigger a function that changes the typography
    elements to textEdits and back. The Modal option is more of a chore but
    definitely easier to handle.

--- components/user-card.jsx
+++ components/user-card.jsx
@@ -1,17 +1,22 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions'
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar'
+import TextField from '@material-ui/core/TextField'
+import IconButton from '@material-ui/core/IconButton'
+import Edit from '@material-ui/icons/Edit'
 
 const useStyles = makeStyles((theme) => ({
   content: {
     display: 'flex',
+    alignItems: 'center'
   },
   info: {
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center'
+    flexGrow: 1
   },
   cover: {
     width: 151,
@@ -23,8 +28,8 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: theme.spacing(1),
   },
   avatar: {
-    width: theme.spacing(7),
-    height: theme.spacing(7),
+    width: theme.spacing(12),
+    height: theme.spacing(12),
     margin: theme.spacing(1)
   },
 }));
@@ -34,24 +39,31 @@ export default function UserCard(props) {
 
   return (
     <Card className={props.className}>
-      <CardContent className={classes.info}>
+      <CardContent className={classes.content}>
+        <div className={classes.info}>
+          <Typography component="h5" variant="h5">
+            Andrés Ricardo Garza Vela
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            aricav96@gmail.com
+          </Typography>
+          <Typography variant="subtitle2" color="textSecondary">
+            8166699777
+          </Typography>
+          <Typography variant="body2">
+            Hijo: Fulano
+          </Typography>
+          <Typography variant="subtitle2">
+            Faltas: 2
+          </Typography>
+        </div>
         <Avatar className={classes.avatar} alt="Ricardo Vela"/>
-        <Typography component="h5" variant="h5">
-          Andrés Ricardo Garza Vela
-        </Typography>
-        <Typography variant="subtitle1" color="textSecondary">
-          aricav96@gmail.com
-        </Typography>
-        <Typography variant="subtitle2" color="textSecondary">
-          8166699777
-        </Typography>
-        <Typography variant="body2">
-          Hijo: Fulano
-        </Typography>
-        <Typography variant="subtitle2">
-          Faltas: 2
-        </Typography>
       </CardContent>
+      <CardActions>
+        <IconButton>
+          <Edit/>
+        </IconButton>
+      </CardActions>
     </Card>
   );
 }
