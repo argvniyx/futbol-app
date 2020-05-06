7e389140da070aa32604fcc34d37766ef9022486
Ricardo Vela <aricav96@gmail.com>
2020-05-06 17:30:04 -0500

    Populate EventDetails (proto)

--- components/dashboard-component.jsx
+++ components/dashboard-component.jsx
@@ -8,6 +8,7 @@ import UserCard from '../components/user-card'
 import EventDetails from '../components/event-details'
 import Directory from '../components/directory'
 import Timeline from '../components/timeline'
+import { useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
   content: {
@@ -46,10 +47,66 @@ let person = {
   children: [son]
 };
 
+const events = [
+  {
+    id: 0,
+    name: "Partido",
+    place: "Uro",
+    date: new Date(2019, 4, 3).toDateString(),
+    hour: "4:30",
+    length: "2hrs",
+    comments: "a"
+  },
+  {
+    id: 1,
+    name: "Entrenamiento",
+    place: "Escuela",
+    date: new Date(2019, 4, 5).toDateString(),
+    hour: "3:30",
+    length: "2hrs",
+    comments: "b"
+  },
+  {
+    id: 2,
+    name: "Junta",
+    place: "Escuela",
+    date: new Date(2019, 4, 5).toDateString(),
+    hour: "5:30",
+    length: "30mins",
+    comments: "c"
+  },
+  {
+    id: 3,
+    name: "Entrenamiento",
+    place: "Escuela",
+    date: new Date(2019, 4, 5).toDateString(),
+    hour: "4:30",
+    length: "2hrs",
+    comments: "d"
+  },
+  {
+    id: 4,
+    name: "Entrenamiento",
+    place: "Uro",
+    date: new Date(2019, 4, 5).toDateString(),
+    hour: "4:30",
+    length: "2hrs",
+    comments: "e"
+  },
+];
+
 const DashboardComponent = props => {
   const classes = useStyles();
   const fixedHeightPaperT = clsx(classes.paper, classes.fixedHeightTop);
   const fixedHeightPaperB = clsx(classes.paper, classes.fixedHeightBottom);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const [currentEvent, setCurrentEvent] = React.useState(events[0]);
+
+  // Handle the event details logic
+  const handleEventDetails = (index) => {
+    setSelectedIndex(index);
+    setCurrentEvent(events[index]);
+  }
 
   return (
     <Box className={classes.root}>
@@ -58,10 +115,10 @@ const DashboardComponent = props => {
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>
             <Grid item xs={12} lg={6}>
-              <Timeline className={fixedHeightPaperT}/>
+              <Timeline className={fixedHeightPaperT} events={events} handler={handleEventDetails}/>
             </Grid>
             <Grid item xs={12} lg={6}>
-              <EventDetails className={fixedHeightPaperT}/>
+              <EventDetails className={fixedHeightPaperT} event={currentEvent}/>
             </Grid>
             <Grid item xs={12} lg={6}>
               <Directory className={fixedHeightPaperB}/>
--- components/event-details.jsx
+++ components/event-details.jsx
@@ -4,16 +4,23 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
 export default function EventDetails(props) {
+  const {name, place, date, hour, length, comments} = props.event;
   return (
     <Card className={props.className}>
       <CardHeader title="Evento"/>
       <CardContent>
         <Typography variant="h6">Nombre de Evento</Typography>
+        {name}
         <Typography variant="h6">Asistencia</Typography>
+        {}
         <Typography variant="h6">Lugar del Evento</Typography>
+        {place}
         <Typography variant="h6">Hora</Typography>
+        {hour}
         <Typography variant="h6">Duración</Typography>
+        {length}
         <Typography variant="h6">Comentarios</Typography>
+        {comments}
       </CardContent>
     </Card>
   );
