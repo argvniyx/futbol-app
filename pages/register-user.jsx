a5a2338b3fb87ca564ec52eef88ca05571e572fb
Ricardo Vela <aricav96@gmail.com>
2020-05-09 18:43:08 -0500

    Add Registration button on Register User page
    (No logic yet)

--- pages/register-user.jsx
+++ pages/register-user.jsx
@@ -1,10 +1,28 @@
 import SideImageForm from '../components/side-image-form'
 import UserTextFields from '../components/user-textfields'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles((theme) => ({
+    submit: {
+        margin: theme.spacing(3, 0, 2)
+    }
+}));
 
 export default function RegisterParent() {
+    const classes = useStyles();
+
     return (
         <SideImageForm imgPath="register-user-image.jpg" title="Registro">
           <UserTextFields/>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            className={classes.submit}
+          >
+            Registrarse
+        </Button>
         </SideImageForm>
     );
 }
