e6858c5d739a84f8ffcf4a4efa79e5d6e3c70349
Ricardo Vela <aricav96@gmail.com>
2020-05-08 21:42:53 -0500

    Abstract common user attributes in form.
    Also, delete the old register user page

--- pages/register-user.jsx
+++ pages/register-user.jsx
@@ -1,76 +1,10 @@
 import SideImageForm from '../components/side-image-form'
-import TextField from '@material-ui/core/TextField'
-import { makeStyles } from '@material-ui/styles'
+import UserTextFields from '../components/user-textfields'
 
-const useStyles = makeStyles((theme) => ({
-    names: {
-        display: "flex",
-        justifyContent: "space-between"
-    }
-}));
-
-export default function RegisterUserTest() {
-    const classes = useStyles();
+export default function RegisterParent() {
     return (
         <SideImageForm imgPath="register-user-image.jpg" title="Registro">
-          <div className={classes.names}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              required
-              id="Nombre"
-              label="Nombre"
-              name="nombre"
-              autoComplete="name"
-            />
-
-            <TextField
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              required
-              id="lastName"
-              label="Apellido"
-              name="apellido"
-              autoComplete="name"
-            />
-          </div>
-
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Correo Electrónico"
-            name="email"
-            autoComplete="email"
-          />
-
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Contraseña"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="phone"
-            label="Teléfono"
-            name="Phone"
-            autoComplete="phone"
-          />
-
+          <UserTextFields/>
         </SideImageForm>
     );
 }
