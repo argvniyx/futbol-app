caec0af596cd07aa2276af14259c7cdf6d54c688
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2020-05-11 22:29:54 -0500

    Added delete and check coach is owner of the team

--- functions/Routes/EventRoutes.js
+++ functions/Routes/EventRoutes.js
@@ -71,6 +71,7 @@ router.put('/:id',
     const Duration = req.body.timeDuration;
     const Description = req.body.Description;
     const eventID = req.params.id;
+    const coachID = req.user_id;
 
     if (
         !Name ||
@@ -83,20 +84,105 @@ router.put('/:id',
         dDate = new Date(dDate);
     }
 
+    // get the coach team
     firestore.collection(
-        'events'
-    ).doc(eventID).update({
-        Name: Name,
-        Place: Place,
-        Date: dDate,
-        Duration: Duration,
-        Description: Description
-    }).then(() => {
-        return res.status(200).send('Event updated successfully');
+        'users'
+    ).doc(coachID).get().then((doc) => {
+        const coachTeam = doc.data().TeamID;
+
+        // get the team of the event
+        firestore.collection(
+            'events'
+        ).doc(eventID).get().then((docEvent) => {
+            const eventTeam = docEvent.data().TeamID;
+
+            if (
+                // verified the coach is owner of the event the team belongs to
+                coachTeam === eventTeam
+            ) {
+
+                // update
+                firestore.collection(
+                    'events'
+                ).doc(eventID).update({
+                    Name: Name,
+                    Place: Place,
+                    Date: dDate,
+                    Duration: Duration,
+                    Description: Description
+                }).then(() => {
+                    return res.status(200).send('Event updated successfully');
+                }).catch((error) => {
+                    return res.status(500).send(error.message);
+                });
+
+            } else {
+                return res.status(400).send("You are not owner of that team");
+            }
+
+        }).catch((error) => {
+            return res.status(500).send(error.message);
+        });
+
     }).catch((error) => {
         return res.status(500).send(error.message);
     });
 
+});
+
+// TODO: Here the get for events related to my team
+
+// ---------------------------------------------------------
+router.delete('/:id',
+    authenticated,
+    isCoach,
+    (req, res) => {
+
+    const firestore = admin.firestore();
+    const coachID = req.user_id;
+    const eventID = req.params.id;
+
+    firestore.collection(
+        'users'
+    ).doc(coachID).get().then((doc) => {
+        const coachTeam = doc.data().TeamID;
+
+        firestore.collection(
+            'events'
+        ).doc(eventID).get().then((docEvent) => {
+            const eventTeam = docEvent.data().TeamID;
+
+            if(coachTeam === eventTeam) {
+
+                firestore.collection(
+                    'teams'
+                ).doc(eventTeam).update({
+                    Events: admin.firestore.FieldValue.arrayRemove(eventID)
+                }).then(() => {
+
+                    firestore.collection(
+                        'events'
+                    ).doc(eventID).delete().then(() => {
+                        return res.status(200).send('Event deleted successfully');
+                    }).catch((error) => {
+                        return res.status(500).send(error.message);
+                    });
+
+                }).catch((error) => {
+                    return res.status(500).send(error.message);
+                });
+
+            } else {
+                return res.status(400).send("You are not owner of that team");
+            }
+
+        }).catch((error) => {
+            return res.status(500).send(error.message);
+        });
+
+    }).catch((error) => {
+        return res.status(500).send(error.message);
+    });
 });
 
 module.exports = router;
