b11d278d50a030db0e28550432e931c37cf8cda7
Ricardo Vela <aricav96@gmail.com>
2020-05-20 16:46:10 -0500

    Substitute data source for directory component
    Now, the directory listing will have information from the server as
    opposed to info from the file

--- components/directory.jsx
+++ components/directory.jsx
@@ -6,15 +6,6 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-function createData(id, firstName, lastName, email, phone) {
-  return {id, firstName, lastName, email, phone};
-}
-
-const rows = [
-    createData(1, 'Ricardo', 'Vela', 'arica96@gmail.com', '811999666'),
-    createData(2, 'Napoleon', 'Lazo', 'napo@gmail.com', '811787878')
-];
-
 export default function Directory(props) {
   const [directory, setDirectory] = React.useState([]);
 
@@ -46,14 +37,14 @@ export default function Directory(props) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {directory.map((row) => (
             <TableRow key={row.id}>
               <TableCell component="th" scope="row">
-                {row.firstName}
+                {row.FirstName}
               </TableCell>
-              <TableCell>{row.lastName}</TableCell>
-              <TableCell>{row.email}</TableCell>
-              <TableCell>{row.phone}</TableCell>
+              <TableCell>{row.LastName}</TableCell>
+              <TableCell>{row.Email}</TableCell>
+              <TableCell>{row.Phone ? row.Phone : null}</TableCell>
             </TableRow>
           ))}
         </TableBody>
